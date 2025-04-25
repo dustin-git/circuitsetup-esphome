8f21d556d1d65b19da8a97f819d7c8e4b96aa2db
jdeglavina <jdeg@sugarmanstudios.com>
2025-04-25 15:47:47 -0400

    Update gdo_text_sensor.h

--- components/secplus_gdo/text_sensor/gdo_text_sensor.h
+++ components/secplus_gdo/text_sensor/gdo_text_sensor.h
@@ -1,21 +1,28 @@
 #pragma once
 
 #include "esphome/components/text_sensor/text_sensor.h"
+#include "esphome/core/component.h" 
 
 namespace esphome {
 namespace secplus_gdo {
 
-class GDOTextSensor : public text_sensor::TextSensor {
- public:
-  void set_sync_state(bool synced) {
-    this->publish_state(synced ? "Synced" : "Not Synced");
-  }
+enum TextSensorType : uint8_t {
+  DEVICE_TYPE,
+  MANUFACTURER,
+};
 
-  void set_type(uint8_t type) { this->type_ = type; }
-  uint8_t get_type() const { return this->type_; }
+class GDOTextSensor : public text_sensor::TextSensor, public Component {
+ public:
+  GDOTextSensor() = default;
+
+  void set_sync_state(bool synced) { this->publish_state(synced ? "Synced" : "Not Synced"); }
+  void set_component_source(const std::string &source) { this->component_source_ = source; }
+  void set_type(TextSensorType type) { this->type_ = type; }
+  TextSensorType get_type() const { return this->type_; }
 
  protected:
-  uint8_t type_;
+  TextSensorType type_;
+  std::string component_source_;
 };
 
 }  // namespace secplus_gdo

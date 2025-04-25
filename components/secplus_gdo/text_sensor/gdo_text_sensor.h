c9e09eda477769acc5908d6f88c5c66e72e51a12
jdeglavina <jdeg@sugarmanstudios.com>
2025-04-25 16:42:28 -0400

    Update gdo_text_sensor.h

--- components/secplus_gdo/text_sensor/gdo_text_sensor.h
+++ components/secplus_gdo/text_sensor/gdo_text_sensor.h
@@ -7,15 +7,16 @@ namespace secplus_gdo {
 
 class GDOTextSensor : public text_sensor::TextSensor {
  public:
-  void set_sync_state(bool synced) {
-    this->publish_state(synced ? "Synced" : "Not Synced");
-  }
+  GDOTextSensor() = default;
 
+  void set_sync_state(bool synced) { this->publish_state(synced ? "Synced" : "Not Synced"); }
+  void set_component_source(const std::string &source) { this->component_source_ = source; }
   void set_type(uint8_t type) { this->type_ = type; }
   uint8_t get_type() const { return this->type_; }
 
  protected:
   uint8_t type_;
+  std::string component_source_;
 };
 
 }  // namespace secplus_gdo
